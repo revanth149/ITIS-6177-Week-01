dba4296c59f0fb7155c82b28495a50b1e85a7e9b
revanth149 <gallarevanth143@gmail.com>
2023-01-21 23:40:42 -0500

    second

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("Hello World"); 
+const http = require('http');
+  
+const port = 8080;
+const host = 'localhost';
+// Creating server 
+const server = http.createServer((req, res) => {
+    // Sending the response
+    res.write("Hello World from nodejs server")
+    res.end();
+})
+  
+
+server.listen(port,host, () => {
+    console.log("Server is Running on", port);
+})
